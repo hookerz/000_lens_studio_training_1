f38cef10ed0cc5a9e1e54c316634052b60581ebd
Hebby Mathew <hebby.mathew@byhook.com>
2020-02-20 11:17:41 -0800

    updated segmentation animation trigger to use touch start and end event;

--- Public/Scripts/InputController.js
+++ Public/Scripts/InputController.js
@@ -1,14 +1,14 @@
 // -----JS CODE-----
 // @input Component.ScriptComponent SegmentationFX
 var faceIndex = 0;
-var startEvent = script.createEvent("SmileStartedEvent");
+var startEvent = script.createEvent("TouchStartEvent");
 startEvent.faceIndex = faceIndex;
 startEvent.bind(function (eventData)
 {
     script.SegmentationFX.api.animate(true);
 });
 
-var finishEvent = script.createEvent("SmileFinishedEvent");
+var finishEvent = script.createEvent("TouchEndEvent");
 finishEvent.faceIndex = faceIndex;
 finishEvent.bind(function (eventData)
 {
